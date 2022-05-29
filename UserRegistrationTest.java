8b38485505845ff8597b6724cefea9f25f362620
narsing17 <narsingp14@gmail.com>
2022-05-29 14:48:53 +0530

    Added parameter

---
+++ UserRegistrationTest.java
@@ -0,0 +1,74 @@
+package com.userregistration;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserRegistrationTest {
+    
+    @Test
+    void givenFirstName_WhenProper_ShouldReturnValid() {
+
+        UserRegistration user = new UserRegistration();
+        boolean result = user.firstName("Amol");
+        assertEquals(true, result);
+
+    }
+
+    @Test
+    void givenLastName_WhenProper_ShouldReturnValid() {
+
+        UserRegistration user = new UserRegistration();
+        boolean result = user.lastName("Ghotale");
+        assertEquals(true, result);
+
+    }
+
+    @Test
+    void givenEmail_WhenProper_ShouldReturnValid() {
+
+        UserRegistration user = new UserRegistration();
+        boolean result = user.email("abc@gmail.com.aa.au");
+        assertEquals(true, result);
+
+
+    }
+
+    @Test
+    void givenMobileNumber_WhenProper_ShouldReturnValid() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.mobileNumber("91 8657169656");
+        assertEquals(true, result);
+
+    }
+
+    @Test
+    void givenRuleOne_WhenProper_ShouldReturnValid() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.ruleOne("abcdefghi");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void givenRuleTwo_WhenProper_ShouldReturnValid() {
+
+        UserRegistration user = new UserRegistration();
+        boolean result = user.ruleTwo("Abcdefghi");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void givenRuleThird_WhenProper_ShouldReturnValid() {
+
+        UserRegistration user = new UserRegistration();
+        boolean result = user.ruleThird("Abcdegh1");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void givenRuleFourth_WhenProper_ShouldReturnValid() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.ruleFourth("Abcdegh1@");
+        assertEquals(true, result);
+    }
+
+}
