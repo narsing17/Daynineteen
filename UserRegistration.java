6a7f10975488a21bf6fec44f28be450af9444a53
narsing17 <narsingp14@gmail.com>
2022-05-29 13:29:29 +0530

    Added Last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -19,10 +19,25 @@ public class UserRegistration {
             System.out.println("Name is invalid, Try with another name.");
         }
     }
+    public void lastName(){
+
+        System.out.println("Enter the last name: ");
+        String name = scanner.next();
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
+        Matcher match = pattern.matcher(name);
+
+        if (match.matches()) {
+            System.out.println("Name is valid");
+        } else {
+            System.out.println("Name is invalid, Try with another name.");
+        }
+    }
 
     public static void main(String[] args) {
 
         UserRegistration check = new UserRegistration();
         check.firstName();
+        check.lastName();
+
     }
 }
