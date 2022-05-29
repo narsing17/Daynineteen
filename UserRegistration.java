219c6fcf22cf772e99f9d816274615a0406641e4
narsing17 <narsingp14@gmail.com>
2022-05-29 13:47:05 +0530

    Added emailid

--- UserRegistration.java
+++ UserRegistration.java
@@ -32,12 +32,24 @@ public class UserRegistration {
             System.out.println("Name is invalid, Try with another name.");
         }
     }
+    public void email() {
 
+        System.out.println("Enter the email: ");
+        String mail = scanner.next();
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+[.][A-Za-z]+@[a-zA-Z]+[.]+[a-zA-Z]{2}+[.][A-Za-z]{2}+$");
+        Matcher match = pattern.matcher(mail);
+
+        if (match.matches()) {
+            System.out.println("email is valid");
+        } else {
+            System.out.println("email is invalid, Try with another name.");
+        }
+    }
     public static void main(String[] args) {
 
         UserRegistration check = new UserRegistration();
         check.firstName();
         check.lastName();
-
+        check.email();
     }
 }
